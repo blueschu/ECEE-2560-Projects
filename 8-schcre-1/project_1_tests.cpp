adeb12d3a90700643776a046b62137cedecbcd1e
Brian Schubert <brianm.schubert@gmail.com>
2020-09-17 20:30:02 -0400

    Update test cases for project 1 to use response class

--- 8-schcre-1/project_1_tests.cpp
+++ 8-schcre-1/project_1_tests.cpp
@@ -19,21 +19,20 @@ namespace {
 struct CodeTestCase {
     Code code;
     Code guess;
-    GuessResponse::Count correct;
-    GuessResponse::Count incorrect;
+    GuessResponse response;
 };
 } // end namespace
 
 int main()
 {
     const auto test_cases = std::array{
-        CodeTestCase{{1, 2, 3}, {1, 2, 3}, 3, 0},
-        CodeTestCase{{1, 3, 2}, {1, 2, 3}, 1, 2},
-        CodeTestCase{{0, 1, 2, 3, 4}, {1, 2, 3, 4, 0}, 0, 5},
-        CodeTestCase{{3, 2, 3, 3, 3}, {1, 3, 3, 4, 5}, 1, 1},
-        CodeTestCase{{1, 2, 3, 4, 5}, {5, 0, 3, 2, 6}, 1, 2},
-        CodeTestCase{{1, 2, 3, 4, 5}, {4, 4, 3, 3, 3}, 1, 1},
-        CodeTestCase{{0, 2, 0, 2, 0}, {1, 2, 0, 4, 2}, 2, 1}
+        CodeTestCase{{1, 2, 3}, {1, 2, 3}, {3, 0}},
+        CodeTestCase{{1, 3, 2}, {1, 2, 3}, {1, 2}},
+        CodeTestCase{{0, 1, 2, 3, 4}, {1, 2, 3, 4, 0}, {0, 5}},
+        CodeTestCase{{3, 2, 3, 3, 3}, {1, 3, 3, 4, 5}, {1, 1}},
+        CodeTestCase{{1, 2, 3, 4, 5}, {5, 0, 3, 2, 6}, {1, 2}},
+        CodeTestCase{{1, 2, 3, 4, 5}, {4, 4, 3, 3, 3}, {1, 1}},
+        CodeTestCase{{0, 2, 0, 2, 0}, {1, 2, 0, 4, 2}, {2, 1}}
     };
 
     std::size_t case_number{0};
@@ -41,14 +40,14 @@ int main()
     for (const CodeTestCase& test_case : test_cases) {
         const auto result = test_case.code.check_guess(test_case.guess);
 
-        if (result.correct_count == test_case.correct && result.incorrect_count == test_case.incorrect) {
+        if (result == test_case.response) {
             std::cout << "Case " << case_number << " OK\n";
         } else {
             std::cout << "Case " << case_number << " FAILED:\n"
                       << "Code:  " << test_case.code << '\n'
                       << "Guess: " << test_case.guess << '\n'
-                      << "Expected C/I:" << test_case.correct << '/' << test_case.incorrect << '\n'
-                      << "Actual C/I:  " << result.correct_count << '/' << result.incorrect_count << '\n';
+                      << "Expected (C,I): " << test_case.response << '\n'
+                      << "Actual (C,I):   " << result << '\n';
         }
         ++case_number;
     }
