88968035732bf03b448bb13e5fcc438b9cbdc6b4
Brian Schubert <brianm.schubert@gmail.com>
2020-12-07 18:29:03 -0500

    Add temporary demo of DFS for proj 5

--- 8-schcre-5/part_b.cpp
+++ 8-schcre-5/part_b.cpp
@@ -7,10 +7,11 @@
  *
  */
 
-#include <array>
-#include <iostream>
+#include <array>                // for std::array
+#include <iostream>             // for I/O stream definitions
 
 #include "maze.h"
+#include "graph_walker.h"
 
 constexpr auto k_maze_files = std::array{
     "resources/maze1.txt",
@@ -20,19 +21,33 @@ constexpr auto k_maze_files = std::array{
 
 int main()
 {
-
     for (const auto file_name : k_maze_files) {
         std::cout << file_name << ":\n";
         const auto maze = Maze::read_file(file_name);
         auto graph = maze.make_graph();
 
-        for (const auto& node : graph) {
-            std::cout << node.index() << ":";
-            for (const auto& neighbor : node.neighbors()) {
-                std::cout << ' ' << neighbor.index();
+        // Uncomment to see the generated graph
+//        for (const auto& node : graph) {
+//            std::cout << '(' << (*node).first << ',' << (*node).second << "):";
+//            for (const auto& neighbor : node.neighbors()) {
+//                std::cout << ' ' << '(' << (*neighbor).first << ',' << (*neighbor).second << ')';
+//            }
+//            std::cout << '\n';
+//        }
+
+        GraphWalker<Maze::Coordinate, Maze::PathWeight> walker;
+        auto path = walker.find_path_dfs(graph, *std::begin(graph), *(std::end(graph) - 1));
+        if (!path.empty()) {
+            std::cout << "Path: ";
+            for (const auto& node : path) {
+                std::cout << '(' << (*node).first << ',' << (*node).second << "), ";
             }
-            std::cout << '\n';
+        } else {
+            std::cout << "No path found.";
         }
-        std::cout << '\n';
+
+
+        std::cout << "\n\n";
     }
+
 }
